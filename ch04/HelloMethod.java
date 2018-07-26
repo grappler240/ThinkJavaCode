db3218c80ad3592fd91376f50f2e5ef47921e22e
Ronnie Stephens <grappler240@aol.com>
2018-07-26 10:12:52 -0500

    added method demo

---
+++ ch04/HelloMethod.java
@@ -0,0 +1,25 @@
+public class HelloMethod
+
+{
+    public static void main(String[] args)
+    {
+
+        printHelloWorld(" Fred");
+        printOhNo();
+        printHelloWorld(" Wilma");
+    }
+
+    public static void printHelloWorld(String name)
+    {
+        System.out.println("Hello World" + name);
+
+    }
+
+    public static void printOhNo()
+    {
+
+        System.out.println("Oh NO!!!!!!!!!!!!!!!!!!!!!");
+    }
+
+
+}
